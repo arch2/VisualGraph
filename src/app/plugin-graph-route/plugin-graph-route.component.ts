745dd261b52e18104d75e9fb786ddffec97de442
arch2 <mattstrattn@gmail.com>
2021-08-09 12:22:01 -0400

    mocked up graph

--- src/app/plugin-graph-route/plugin-graph-route.component.ts
+++ src/app/plugin-graph-route/plugin-graph-route.component.ts
@@ -1,15 +1,190 @@
 import { Component, OnInit } from '@angular/core';
+import { ClusterNode, Edge, Node } from '@swimlane/ngx-graph';
 
 @Component({
   selector: 'app-plugin-graph-route',
   templateUrl: './plugin-graph-route.component.html',
   styleUrls: ['./plugin-graph-route.component.scss']
 })
-export class PluginGraphRouteComponent implements OnInit {
-  config: any = "Hello World";
-  constructor() { }
-
-  ngOnInit(): void {
+export class PluginGraphRouteComponent {
+  nodes: Node[];
+  clusters: ClusterNode[] = [];
+  links: Edge[];
+  constructor() {
+    this.nodes =
+      [
+        {
+          id: 'REQ1',
+          label: 'REQ 1'
+        },
+        {
+          id: 'REL1',
+          label: 'REL'
+        },
+        {
+          id: 'AO',
+          label: 'AO'
+        },
+        {
+          id: 'PA',
+          label: 'PA'
+        },
+        {
+          id: 'PEER',
+          label: 'PEER',
+        },
+        {
+          id: 'IT',
+          label: 'IT',
+        },
+        {
+          id: 'RMLRO',
+          label: 'RML RO',
+        },
+        {
+          id: 'NBS',
+          label: 'NBS',
+        },
+        {
+          id: 'PRISM',
+          label: 'PRISM',
+        },
+        {
+          id: 'REQ2',
+          label: 'REQ 2',
+        },
+        {
+          id: 'NBSRO',
+          label: 'NBS RO',
+        },
+        {
+          id: 'ARCHPA',
+          label: 'ARCH PA',
+        }
+      ];
+    this.links =
+      [
+        {
+          id: 'a',
+          source: 'REQ1',
+          target: 'REL1',
+          label: 'next step'
+        },
+        {
+          id: 'b',
+          source: 'REL1',
+          target: 'REQ1',
+          label: 'prev step'
+        },
+        {
+          id: 'c',
+          source: 'REL1',
+          target: 'AO',
+          label: 'NEXT'
+        },
+        {
+          id: 'd',
+          source: 'AO',
+          target: 'REL1',
+          label: 'PREV'
+        },
+        {
+          id: 'e',
+          source: 'AO',
+          target: 'PA',
+          label: 'NEXT'
+        },
+        {
+          id: 'f',
+          source: 'PA',
+          target: 'AO',
+          label: 'PREV'
+        },
+        {
+          id: 'g',
+          source: 'PA',
+          target: 'REQ1',
+          label: 'PREV'
+        },
+        {
+          id: 'h',
+          source: 'PA',
+          target: 'PEER',
+          label: 'NEXT'
+        },
+        {
+          id: 'i',
+          source: 'PEER',
+          target: 'PA',
+          label: 'PREV'
+        },
+        {
+          id: 'j',
+          source: 'PA',
+          target: 'RMLRO',
+          label: 'NEXT'
+        },
+        {
+          id: 'k',
+          source: 'PA',
+          target: 'IT',
+          label: 'NEXT'
+        },
+        {
+          id: 'l',
+          source: 'IT',
+          target: 'PA',
+          label: 'PREV'
+        },
+        {
+          id: 'm',
+          source: 'PA',
+          target: 'NBS',
+          label: 'NEXT'
+        },
+        {
+          id: 'n',
+          source: 'NBS',
+          target: 'PRISM',
+          label: 'NEXT'
+        },
+        {
+          id: 'o',
+          source: 'PRISM',
+          target: 'REQ2',
+          label: 'NEXT'
+        },
+        {
+          id: 'p',
+          source: 'REQ2',
+          target: 'NBSRO',
+          label: 'NEXT'
+        },
+        {
+          id: 'q',
+          source: 'NBSRO',
+          target: 'ARCHPA',
+          label: 'NEXT'
+        },
+        {
+          id: 'r',
+          source: 'ARCHPA',
+          target: 'NBSRO',
+          label: 'PREV'
+        },
+        {
+          id: 's',
+          source: 'ARCHPA',
+          target: 'PA',
+          label: 'NEXT'
+        },
+        {
+          id: 't',
+          source: 'PA',
+          target: 'ARCHPA',
+          label: 'PREV'
+        }
+      ];
   }
 
 }
--- src/app/plugin-graph/plugin-graph.component.ts
+++ src/app/plugin-graph/plugin-graph.component.ts
@@ -1,15 +1,118 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import * as shape from 'd3-shape';
+import { Edge, Node, ClusterNode, Layout } from '@swimlane/ngx-graph';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-plugin-graph',
   templateUrl: './plugin-graph.component.html',
   styleUrls: ['./plugin-graph.component.scss']
 })
 export class PluginGraphComponent implements OnInit {
-  @Input() config: any;
-  constructor() { }
+  name = 'NGX-Graph Demo';
+  @Input() nodes: Node[] = [];
+  @Input() clusters: ClusterNode[] = [];
+  @Input() links: Edge[] = [];
 
-  ngOnInit(): void {
+  layout: string | Layout = 'dagre';
+  layouts: any[] = [
+    {
+      label: 'Dagre',
+      value: 'dagre',
+    },
+    {
+      label: 'Dagre Cluster',
+      value: 'dagreCluster',
+      isClustered: true,
+    },
+    {
+      label: 'Cola Force Directed',
+      value: 'colaForceDirected',
+      isClustered: true,
+    },
+    {
+      label: 'D3 Force Directed',
+      value: 'd3ForceDirected',
+    },
+  ];
+
+
+  // line interpolation
+  curveType: string = 'Bundle';
+  curve: any = shape.curveLinear;
+  interpolationTypes = [
+    'Bundle',
+    'Cardinal',
+    'Catmull Rom',
+    'Linear',
+    'Monotone X',
+    'Monotone Y',
+    'Natural',
+    'Step',
+    'Step After',
+    'Step Before'
+  ];
+
+  draggingEnabled: boolean = false;
+  panningEnabled: boolean = false;
+  zoomEnabled: boolean = false;
+
+  zoomSpeed: number = 0.1;
+  minZoomLevel: number = 0.1;
+  maxZoomLevel: number = 4.0;
+  panOnZoom: boolean = true;
+
+  autoZoom: boolean = true;
+  autoCenter: boolean = true;
+
+  update$: Subject<boolean> = new Subject();
+  center$: Subject<boolean> = new Subject();
+  zoomToFit$: Subject<boolean> = new Subject();
+
+  ngOnInit() {
+    this.setInterpolationType(this.curveType);
   }
 
+  setInterpolationType(curveType: any) {
+    this.curveType = curveType;
+    if (curveType === 'Bundle') {
+      this.curve = shape.curveBundle.beta(1);
+    }
+    if (curveType === 'Cardinal') {
+      this.curve = shape.curveCardinal;
+    }
+    if (curveType === 'Catmull Rom') {
+      this.curve = shape.curveCatmullRom;
+    }
+    if (curveType === 'Linear') {
+      this.curve = shape.curveLinear;
+    }
+    if (curveType === 'Monotone X') {
+      this.curve = shape.curveMonotoneX;
+    }
+    if (curveType === 'Monotone Y') {
+      this.curve = shape.curveMonotoneY;
+    }
+    if (curveType === 'Natural') {
+      this.curve = shape.curveNatural;
+    }
+    if (curveType === 'Step') {
+      this.curve = shape.curveStep;
+    }
+    if (curveType === 'Step After') {
+      this.curve = shape.curveStepAfter;
+    }
+    if (curveType === 'Step Before') {
+      this.curve = shape.curveStepBefore;
+    }
+  }
+
+  setLayout(layoutName: string): void {
+    const layout = this.layouts.find(l => l.value === layoutName);
+    this.layout = layoutName;
+    if (!layout.isClustered) {
+      this.clusters = undefined as any;
+    } else {
+      this.clusters = this.clusters;
+    }
+  }
 }
