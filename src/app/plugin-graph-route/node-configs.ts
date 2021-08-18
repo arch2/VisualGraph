5b1a45dc732aba5391b0dce1f030a760765e5faa
arch2 <mattstrattn@gmail.com>
2021-08-18 14:20:09 -0400

    create constructor for defaults

--- src/app/plugin-graph-route/node-configs.ts
+++ src/app/plugin-graph-route/node-configs.ts
@@ -1,36 +1,82 @@
 import { DataSet } from "vis-data";
 import { Edge, Node } from 'vis-network';
 
+class internalNode {
+    id: string;
+    label: string;
+    x: number;
+    y: number;
+    fixed: boolean;
+    color: any;
+    shape: any;
+    shapeProperties: any
+    widthConstraint: any;
+    heightConstraint: any;
+    constructor(options: {
+        id?: string,
+        label?: string,
+        x?: number,
+        y?: number,
+        fixed?: boolean,
+        active?: boolean,
+    } = {}) {
+        this.id = options.id ? options.id : Math.random().toString();
+        this.label = options.label ? options.label : "Default Label";
+        this.x = options.x ? options.x : 0;
+        this.y = options.y ? options.y : 0;
+        this.fixed = options.fixed ? options.fixed : true;
+        const nodeColor = options.active ? 'red' : '#333333'
+        const backgroundColor = '#FFFFFF';
+        this.color = {
+            border: nodeColor,
+            background: backgroundColor,
+            highlight: { background: backgroundColor, border: nodeColor }
+        };
+        this.shape = 'box';
+        this.shapeProperties = { borderRadius: 20 };
+        this.widthConstraint = { minimum: 50 };
+        this.heightConstraint = { minimum: 25 };
+    }
+}
+// class internalEdge {
+//     id: string;
+//     from: string;
+//     to: string;
+//     arrows: any;
+//     constructor(options: {} = {}) {
+
+//     }
+// }
 export const graphConfigs: any[] = [
     {
         configName: "A",
-        nodes: new DataSet<Node>([
-            { id: 'REQ1', label: 'REQ 1', x: 0, y: 0, fixed: true },
-            { id: 'REL1', label: 'REL', x: 150, y: 0, fixed: true },
-            { id: 'AO', label: 'AO', x: 300, y: 0, fixed: true },
-            { id: 'PA', label: 'PA', x: 450, y: 0, fixed: true, color: "red" },
-            { id: 'PEER', label: 'PEER', x: 450, y: -100, fixed: true },
-            { id: 'IT', label: 'IT', x: 300, y: 100, fixed: true },
-            { id: 'RMLRO', label: 'RML RO', x: 450, y: 100, fixed: true },
-            { id: 'NBS', label: 'NBS', x: 600, y: 0, fixed: true },
-            { id: 'PRISM', label: 'PRISM', x: 750, y: 0, fixed: true },
-            { id: 'REQ2', label: 'REQ 2', x: 900, y: 0, fixed: true },
-            { id: 'NBSRO', label: 'NBS RO', x: 1050, y: 0, fixed: true },
-            { id: 'ARCHPA', label: 'ARCH PA', x: 1200, y: 0, fixed: true }
-        ]),
-        edges: new DataSet<any>([
+        nodes: [
+            new internalNode({ id: 'REQ1', label: 'REQ 1', x: 0, y: 0 }),
+            new internalNode({ id: 'REL1', label: 'REL', x: 150, y: 0 }),
+            new internalNode({ id: 'AO', label: 'AO', x: 300, y: 0 }),
+            new internalNode({ id: 'PA', label: 'PA', x: 450, y: 0, active: true }),
+            new internalNode({ id: 'PEER', label: 'PEER', x: 450, y: -100 }),
+            new internalNode({ id: 'IT', label: 'IT', x: 300, y: 100 }),
+            new internalNode({ id: 'RMLRO', label: 'RML RO', x: 450, y: 100 }),
+            new internalNode({ id: 'NBS', label: 'NBS', x: 600, y: 0 }),
+            new internalNode({ id: 'PRISM', label: 'PRISM', x: 750, y: 0 }),
+            new internalNode({ id: 'REQ2', label: 'REQ 2', x: 900, y: 0 }),
+            new internalNode({ id: 'NBSRO', label: 'NBS RO', x: 1050, y: 0 }),
+            new internalNode({ id: 'ARCHPA', label: 'ARCH PA', x: 1200, y: 0 }),
+        ],
+        edges: [
             { id: 'a', from: 'REQ1', to: 'REL1', arrows: "to", },
             { id: 'b', from: 'REL1', to: 'REQ1', arrows: "to", },
             { id: 'c', from: 'REL1', to: 'AO', arrows: "to", dashes: true },
             { id: 'd', from: 'AO', to: 'REL1', arrows: "to", dashes: true },
             { id: 'e', from: 'AO', to: 'PA', arrows: "to", dashes: true },
             { id: 'f', from: 'PA', to: 'AO', arrows: "to", dashes: true },
-            { id: 'g', from: 'PA', to: 'REQ1', arrows: "to", smooth: { type: "curvedCW", roundness: 0.2 } },
+            { id: 'g', from: 'PA', to: 'REQ1', arrows: { to: { enabled: true, scaleFactor: 0.7 } }, smooth: { type: "curvedCW", roundness: 0.2 } },
             { id: 'h', from: 'PA', to: 'PEER', arrows: "to", },
             { id: 'i', from: 'PEER', to: 'PA', arrows: "to", },
             { id: 'j', from: 'PA', to: 'RMLRO', arrows: "to", },
-            { id: 'k', from: 'PA', to: 'IT', arrows: "to", },
-            { id: 'l', from: 'IT', to: 'PA', arrows: "to", },
+            // { id: 'k', from: 'PA', to: 'IT', arrows: "to", },
+            { id: 'l', from: 'PA', to: 'IT', arrows: { to: { enabled: true, scaleFactor: 0.7 }, from: { enabled: true, scaleFactor: 0.7 } }, smooth: { type: "curvedCW", roundness: 0.2 } },
             { id: 'm', from: 'PA', to: 'NBS', arrows: "to", },
             { id: 'n', from: 'NBS', to: 'PRISM', arrows: "to", },
             { id: 'o', from: 'PRISM', to: 'REQ2', arrows: "to", },
@@ -39,7 +85,7 @@ export const graphConfigs: any[] = [
             { id: 'r', from: 'ARCHPA', to: 'NBSRO', arrows: "to", },
             { id: 's', from: 'ARCHPA', to: 'PA', arrows: "to", smooth: { type: "curvedCW", roundness: 0.2 } },
             { id: 't', from: 'PA', to: 'ARCHPA', arrows: "to,from", smooth: { type: "curvedCW", roundness: 0.2 } }
-        ]),
+        ],
     },
     {
         configName: "B",
